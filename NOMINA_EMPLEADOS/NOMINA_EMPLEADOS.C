433e844d49ba893c25f085a5854c058e6c33b202
Lourdes-Belen <barbagelata.lourdes.belen@gmail.com>
2025-05-14 05:01:52 +0000

    Subo las estructuras del ejercicio 1

---
+++ NOMINA_EMPLEADOS/NOMINA_EMPLEADOS.C
@@ -0,0 +1,1 @@
+#include "NOMINA_EMPLEADOS.H"
---
+++ NOMINA_EMPLEADOS/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "NOMINA_EMPLEADOS.H"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
